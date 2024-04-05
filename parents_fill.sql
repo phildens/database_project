637512e1d04c5129e925db41676adc7c1a1e4a4e
phildens <vborodulin67@2gmail.com>
2024-04-05 17:06:41 +0500

    add data to parents

---
+++ parents_fill.sql
@@ -0,0 +1,6 @@
+
+INSERT INTO parents (name, surname, email_adress)
+VALUES
+    ('Jane', 'Smith', 'jane.smith@example.com'),
+    ('Michael', 'Johnson', 'michael.johnson@example.com'),
+    ('Emily', 'Brown', 'emily.brown@example.com');
